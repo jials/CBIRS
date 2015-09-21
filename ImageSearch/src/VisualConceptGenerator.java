a90b8e08480b07c88fe98e4ebf5774cbf3ed4b2e
jiale <jiale@comp.nus.edu.sg>
2015-09-21 10:32:48 +0800

    Added FilepathsGenerator
    Automatically generate filepaths of jpg

---
+++ ImageSearch/src/VisualConceptGenerator.java
@@ -0,0 +1,29 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class VisualConceptGenerator {
+	public void classifyInputImage(String filePath) {
+		BufferedWriter bw;
+		try {
+			bw = new BufferedWriter(new FileWriter("SemanticFeature/input.txt"));
+			bw.write(filePath);
+			bw.close();
+		} catch (IOException e) {
+			System.out.println("Error writing filepath of image into SemanticFeature/input.txt");
+		}
+		
+		try {
+			Process classification = new ProcessBuilder(
+					"SemanticFeature/image_classification.exe", "input.txt")
+					.start();
+		} catch (IOException e) {
+			System.out.println("Error calling image_classification.exe");
+		}
+		
+	}
+	
+	
+}
---
+++ ImageSearch/src/learning/FilepathsGenerator.py
@@ -0,0 +1,41 @@
+import sys
+import os
+import getopt
+
+def build(training_dir, output_file):
+	filepaths = []
+
+	for root, dirs, files in os.walk(training_dir):
+		for file in files:
+			if file.endswith(".jpg"):
+				filepaths.append(os.path.join(root, file))
+
+	with open(output_file, "w+") as f:
+		for filepath in filepaths:
+			f.write(filepath + "\n")
+
+	print "Successfully generated filepaths of img files in this directory!"
+
+
+def usage():
+	print "usage: " + sys.argv[0] + " -t training-dir -c output.txt"
+
+
+training_i = output = None
+try:
+	opts, args = getopt.getopt(sys.argv[1:], 't:c:')
+except getopt.GetoptError, err:
+	usage()
+	sys.exit(2)
+for o,a in opts:
+	if o == '-t':
+		training_i = a
+	elif o == '-c':
+		output = a
+	else:
+		assert False, "unhandled option"
+if training_i is None or output is None:
+	usage()
+	sys.exit(2)
+
+build(training_i, output)
