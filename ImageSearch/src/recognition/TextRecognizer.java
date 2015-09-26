f012655fc881e36fd8a7bceb09dc4730f58de1db
Chan Jun Wei <chanjunweimy@gmail.com>
2015-09-26 17:15:55 +0800

    Integrating Text Retrieval feature for GUI
    Signed-off-by: Chan Jun Wei <chanjunweimy@gmail.com>

---
+++ ImageSearch/src/recognition/TextRecognizer.java
@@ -0,0 +1,9 @@
+package recognition;
+
+public class TextRecognizer {
+	private static final String PATH_DB_TEXT = "ImageData/train/";
+	private static final String PATH_TEST_TEXT = "ImageData/test/";
+	private static final String TEXT_INVERTED_DB_NANE = "inverted_train_tags.txt";
+	private static final String TEXT_TEST_NAME = "test_tags.txt";
+	
+}
